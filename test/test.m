5a5cdd3660cef91a58585712307e18f406ccd4fa
sugiuta <sugi.uta_1203@icloud.com>
2020-05-11 21:03:18 +0900

    test

--- test/test.m
+++ test/test.m
@@ -3,49 +3,29 @@
 
 @implementation test
 
-- (id)specifiers {
+-(id)specifiers {
 	if (_specifiers == nil) {
 		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
 	}
 	return _specifiers;
 }
 
-- (instancetype)init {
+-(instancetype)init {
     self = [super init];
 
     if (self) {
-        HBAppearanceSettings *appearanceSettings = [[HBAppearanceSettings alloc] init];
-        appearanceSettings.tintColor = [UIColor colorWithRed:1.0f green:0.1f blue:0.1f alpha:1];
-        appearanceSettings.tableViewCellSeparatorColor = [UIColor colorWithRed:0.21 green:0.21 blue:0.31 alpha:1.0];
-        self.hb_appearanceSettings = appearanceSettings;
-        self.respringButton = [[UIBarButtonItem alloc] initWithTitle:@"Apply" 
+self.respringButton = [[UIBarButtonItem alloc] initWithTitle:@"Apply" 
                                     style:UIBarButtonItemStylePlain
-                                    target:self 
-                                    action:@selector(respring:)];
-        self.respringButton.tintColor = [UIColor redColor];
-        self.navigationItem.rightBarButtonItem = self.respringButton;
+target:self
+action:@selector(respring:)];
+
+self.navigationItem.rightBarButtonItem = self.respringButton;
     }
 
     return self;
 }
 
--(void) contactMethod
-{
-UIAlertView *alert1 = [[UIAlertView alloc]initWithTitle:@"Twitter ID" message:@"sugi.uta @uta_sugi" delegate:self cancelButtonTitle:@"Open Twitter" otherButtonTitles:nil];
-[alert1 show];
-}
-
--(void)alertView:(UIAlertView *)alertView  clickedButtonAtIndex:(NSInteger)buttonIndex {
-NSString *button = [alertView buttonTitleAtIndex:buttonIndex];
-
-if([button isEqualToString:@"Open Twitter"])
-{//Open Twitterボタンを押した場合の対応
-[[UIApplication sharedApplication]openURL:[NSURL URLWithString:@"https://mobile.twitter.com/uta_sugi"]];
-}
-}
-
 - (void)respring:(id)sender {
-
 UIViewController *view = [UIApplication sharedApplication].keyWindow.rootViewController;
         while (view.presentedViewController != nil && !view.presentedViewController.isBeingDismissed) {
                 view = view.presentedViewController;
@@ -70,32 +50,6 @@ UIViewController *view = [UIApplication sharedApplication].keyWindow.rootViewCon
     [view presentViewController:alertController animated:YES completion:nil];
 
 }
-
-
--(void)github {
-		 	 			[[UIApplication sharedApplication]
-		 	 			openURL:[NSURL URLWithString:@"https://github.com/sugiuta/tutorial"]
-		 	 			options:@{}
-		 	 			completionHandler:nil];
-		 	 				}
-
--(void)paypal {
-		 	 			[[UIApplication sharedApplication]
-		 	 			openURL:[NSURL URLWithString:@"https://www.paypal.me/sugiuta1203"]
-		 	 			options:@{}
-		 	 			completionHandler:nil];
-		 	 				}
-
-- (void)viewWillAppear:(BOOL)animated {
-    [super viewWillAppear:animated];
-
-    CGRect frame = self.table.bounds;
-    frame.origin.y = -frame.size.height;
-
-    [self.navigationController.navigationController.navigationBar setShadowImage: [UIImage new]];
-    self.navigationController.navigationController.navigationBar.translucent = YES;
-}
-
 
 -(void)showExplanation:(UIAlertController *)expController {
 
